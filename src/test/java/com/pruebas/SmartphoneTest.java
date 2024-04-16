5e16479c984177f0ea7efbec3d8da5e6dd9fe01d
haitz <a@a.a>
2024-04-16 13:09:24 +0200

    BeforeEach y AfterEach explicados y utilizados

--- src/test/java/com/pruebas/SmartphoneTest.java
+++ src/test/java/com/pruebas/SmartphoneTest.java
@@ -2,13 +2,35 @@ package com.pruebas;
 
 import com.example.demo.service.SmartPhoneServiceImpl;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
 import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 
 public class SmartphoneTest {
 
+
+
+    @AfterEach
+    public void after(){
+        System.out.println("@AfterEach");
+    }
+
+    @BeforeEach
+    public void before(){
+        System.out.println("@BeforeEach");
+    }
+
+
+
+
+
+
     @Test
+    @DisplayName("Test para la funcion count")
     public void count(){
         SmartPhoneServiceImpl spsi= new SmartPhoneServiceImpl();
         assertNotNull(spsi.count());
@@ -18,12 +40,14 @@ public class SmartphoneTest {
     }
 
     @Test
+    @DisplayName("Test para la funcion findOne")
     public void findOne(){
         SmartPhoneServiceImpl spsi = new SmartPhoneServiceImpl();
         assertThrows(IllegalArgumentException.class, () -> spsi.findOne(null));
     }
 
     @Test
+    @DisplayName("Test para la funcion count, usando assertAll")
     public void countAssertAll(){
         SmartPhoneServiceImpl spsi = new SmartPhoneServiceImpl();
         assertAll(
@@ -33,5 +57,12 @@ public class SmartphoneTest {
         );
     }
 
-    //Una funcion lambda crea funciones personalizadas reutilizables y asigna un nombre
+    //Una funcion lambda ayuda a referenciar métodos anonimos o sin nombre, ayudando a escribir un codigo mas claro.
+
+    //La opción BeforeEach se utiliza para escoger un metodo que debe ejecutarse antes de cada metodo de la clase.
+    //La opción AfterEach se utiliza para escoger un metodo que debe ejecutarse despues de cada metodo de la clase.
+    //Con estas dos opciones nos podemos asegurar de tener un codigo y entorno mas controlado.
+
+    //La opcion BeforeAll se utiliza par
+
 }
